2b2b07a12cf97038d5adc018e7fb453848df9965
ofirisakov <ofirfifi@gmail.com>
2020-11-25 21:46:31 +0200

    Add simple test cases for the gRPC server

--- payeet/app/bin/app.dart
+++ payeet/app/bin/app.dart
@@ -14,17 +14,63 @@ Future<void> main(List<String> args) async {
   final stub = payeetClient(channel);
 
   try {
-    
-    final response = await stub.login(
-      LoginRequest()
-      ..mail = 'mail'
-      ..password = 'password'
-
-      );
-
-    print('Client received: ${response}');
+    print(await sendRegisterRequest(stub));
+    print(await sendLoginRequest(stub));
+    print(await sendGetBalanceRequest(stub));
+    print(await sendTransferRequest(stub));
   } catch (e) {
     print('Caught error: $e');
   }
   await channel.shutdown();
+}
+
+Future<String> sendLoginRequest(payeetClient stub) async {
+    print('Sending login request...');
+
+    final response = await stub.login(
+      LoginRequest()
+      ..mail = 'mail'
+      ..password = 'password'
+      );
+
+    return 'Login response recieved:\n${response}';
+}
+
+Future<String> sendRegisterRequest(payeetClient stub) async {
+    print('Sending register request...');
+
+    final response = await stub.register(
+      RegisterRequest()
+      ..firstName = 'israel'
+      ..lastName = 'israeli'
+      ..mail = 'mail'
+      ..password = 'password'
+      );
+
+    return 'Register response recieved:\n${response}';
+}
+
+Future<String> sendGetBalanceRequest(payeetClient stub) async {
+    print('Sending getBalance request...');
+
+    final response = await stub.getBalance(
+      BalanceRequest()
+      ..userID = 'SuperCoolThigyMadeOutOfUUID'
+      ..session = 'ThisIsARandomSetOfBytesOrIsThisThingDepricated?'
+      );
+
+    return 'GetBalance response recieved:\n${response}';
+}
+
+Future<String> sendTransferRequest(payeetClient stub) async {
+    print('Sending sendTransfer request...');
+
+    final response = await stub.transferBalance(
+      TransferRequest()
+      ..receiverMail = 'NotIsraelNotIsraeli@abroad.org'
+      ..senderID = 'SuperCoolThigyMadeOutOfUUID'
+      ..session = 'ThisIsARandomSetOfBytesOrIsThisThingDepricated?'
+      );
+
+    return 'SendTransfer response recieved:\n${response}';
 }
