943edb21d126a6165490490ea5cad0d76a322193
Yakir Oren <yakiroren@gmail.com>
2020-12-13 22:03:47 +0200

    changes to the complier.

--- payeet/protos/compile.bat
+++ payeet/protos/compile.bat
@@ -1,2 +1,10 @@
-protoc --proto_path=. --proto_path=third_party --gogo_out=plugins=grpc:go payeet.proto
+@echo off
+echo Compiling go
+echo ----------------
+rem protoc --proto_path=. --proto_path=third_party --gogo_out=plugins=grpc:go payeet.proto
+
+protoc --go_out=plugins=grpc:go payeet.proto
+
+echo Compiling dart
+echo ----------------
 protoc --dart_out=grpc:dart  --proto_path=. -I . payeet.proto
