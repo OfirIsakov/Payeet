2483903abfecc748c81e4681344223ab5b127be2
Ofir Isakov <ofirfifi@gmail.com>
2021-01-07 00:21:42 +0200

    Create a dart client class that has a channel, 2 client(authed and unauthed) and the functions to send to the server

--- payeet/app/bin/app.dart
+++ payeet/app/bin/app.dart
@@ -1,120 +1,63 @@
 import 'package:grpc/grpc.dart';
 
-import '../../protos/dart/payeet.pb.dart';
-import '../../protos/dart/payeet.pbgrpc.dart';
-import 'payeetChannel.dart';
-
-//TODO: Implement a client class to save the refreshToken and do the client logic
+import 'payeetClient.dart';
 
 Future<void> main(List<String> args) async {
-  
-  final channel = PayeetChannel(
-    ClientChannel(
-    'localhost',
-    port: 6969,
-    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
-    ),
+  var client = PayeetClient(
+    PayeetChannel(
+      ClientChannel(
+        'localhost',
+        port: 6969,
+        options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+      ),
+    )
   );
   
-  final authStub = payeet_authClient(channel);
+  client.createUnauthenticatedClient();
 
   try {
     print('---------------------------------------------------------------');
 
     print('sendRegisterRequest...');
-    print('Register response recieved:\n${await sendRegisterRequest(authStub)}');
+    print('Register response recieved:\n${await client.register()}');
 
     print('---------------------------------------------------------------');
 
-    // when logging in we get the access token and we need to send it with every request
+    // when logging in we get the auth tokens and we need to send the access token with every request
     print('sendLoginRequest...');
-    var user = await sendLoginRequest(authStub); 
+    var user = await client.login(); 
     print('Login response recieved:\n${user}');
 
-    // once we get the token we can attach it to the payeetChannel type we implemented
-    channel.addAccessTokenMetadata(user.accessToken);
+    // once we get the tokens we can attach it to the payeetClient's channel we implemented
+    client.accessToken = user.accessToken;
+    client.refreshToken = user.refreshToken;
+    client.createAuthenticatedClient(); // create the authenticated channel
 
     print('---------------------------------------------------------------');
 
     print('sendRefreshRequest...');
-    user = await sendRefreshRequest(authStub, user.refreshToken);
+    user = await client.refreshAccessToken();
     print('RefreshToken response recieved:\n${user}');
 
     print('---------------------------------------------------------------');
-    final normalStub = payeetClient(channel);
     
 
     print('sendBalanceRequest...');
-    print('Balance response recieved:\n${await sendBalanceRequest(normalStub)}');
+    print('Balance response recieved:\n${await client.getBalance()}');
 
     print('---------------------------------------------------------------');
 
     print('sendUserInfoRequest...');
-    print('UserInfo response recieved:\n${await sendUserInfoRequest(normalStub)}');
+    print('UserInfo response recieved:\n${await client.getUserInfo()}');
 
     print('---------------------------------------------------------------');
 
     print('sendTransferRequest...');
-    print('Transfer response recieved:\n${await sendTransferRequest(normalStub)}');
+    print('Transfer response recieved:\n${await client.transferBalance('To@who.com', 1337.69)}');
 
     print('---------------------------------------------------------------');
   } catch (e) {
     print('Caught error: $e');
   }
-  await channel.shutdown();
-}
-
-Future<LoginResponse> sendLoginRequest(payeet_authClient stub) async {
-    final response = await stub.login(
-      LoginRequest()
-      ..mail = 'israel@israeli.co.il'
-      ..password = '2hard2guess'
-      );
-
-    return response;
-}
-
-Future<StatusResponse> sendRegisterRequest(payeet_authClient stub) async {
-    final response = await stub.register(
-      RegisterRequest()
-      ..firstName = 'israel'
-      ..lastName = 'israeli'
-      ..mail = 'israel@israeli.co.il'
-      ..password = '2hard2guess'
-      );
-
-    return response;
-}
-
-Future<LoginResponse> sendRefreshRequest(payeet_authClient stub, String refreshToken) async {
-    final response = await stub.refreshToken(
-      RefreshTokenRequest()
-      ..refreshToken = refreshToken
-      );
-
-    return response;
-}
-
-Future<BalanceResponse> sendBalanceRequest(payeetClient stub) async {
-    final response = await stub.getBalance(
-      BalanceRequest()
-      );
-
-    return response;
-}
-
-Future<UserInfoResponse> sendUserInfoRequest(payeetClient stub) async {
-    final response = await stub.getUserInfo(
-      UserInfoRequest()
-      );
-
-    return response;
-}
-
-Future<StatusResponse> sendTransferRequest(payeetClient stub) async {
-    final response = await stub.transferBalance(
-      TransferRequest()
-      );
-
-    return response;
+  await client.channel.shutdown();
 }
